1ce47ada8af07813e051c0a564728dafd3569850
drussilla <drussilla@ezcodeit.com>
2024-09-29 20:05:39 -0300

    Novo endpoint para calcular o quadrado de um número inteiro.

--- src/main.py
+++ src/main.py
@@ -7,12 +7,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     """
     Função que retorna a mensagem de boas-vindas.
     """
-    return {"message": "Bem-vindo à API de datas!"}
+    return {"message": "Bem-vindo à API!"}
+
 
 @app.get("/data_atual")
 def get_data_atual():
@@ -20,3 +22,14 @@ def get_data_atual():
     Função que retorna a data e hora atuais.
     """
     return {"data_atual": datetime.now().strftime("%d/%m/%Y - %H:%M")}
+
+
+@app.get("/quadrado")
+def quadrado(x: int = None):
+    """
+    Função que retorna o quadrado de um inteiro.
+    """
+    if x is not None:
+        return {"quadrado": x * x}
+    else:
+        return {"message": "x não encontrado no query param"}
